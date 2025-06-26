6b58994ab8f1906d23731d5261faa2fddd585cb0
Kajal Gupta <165830057+ketchup269@users.noreply.github.com>
2025-06-26 22:26:39 +0530

    Delete dist directory

--- dist/config/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    port: process.env.PORT || 4000,
-    geminiApiKey: process.env.GEMINI_API_KEY,
-    geminiModel: process.env.GEMINI_MODEL || 'gemini-2.0-flash-lite',
-    nodeEnv: process.env.NODE_ENV || 'Production',
-};
